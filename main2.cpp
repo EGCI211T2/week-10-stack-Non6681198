4f9998660fb102082fe73c3e39361acbb1717ffd
Non6681198 <preeranat.sri@student.mahidol.edu>
2025-12-12 16:26:04 +0000

    Assignment

--- main2.cpp
+++ main2.cpp
@@ -3,6 +3,7 @@
 #define CHARSTACK_H
 
 #include <iostream>
+#include <string> 
 using namespace std;
 
 class CharNode {
@@ -16,6 +17,18 @@ public:
 class CharStack {
 private:
     CharNode* top;
+
+    bool isMatchingPair(char char1, char char2) {
+        if (char1 == '(' && char2 == ')')
+            return true;
+        else if (char1 == '{' && char2 == '}')
+            return true;
+        else if (char1 == '[' && char2 == ']')
+            return true;
+        else
+            return false;
+    }
+
 public:
     CharStack() : top(nullptr) {}
     ~CharStack() {
@@ -26,20 +39,61 @@ public:
         CharNode* n = new CharNode(c);
         n->next = top;
         top = n;
+       
     }
 
+  
     char pop() {
         if (!top) return '\0';
         char c = top->data;
         CharNode* t = top;
         top = top->next;
         delete t;
-        cout << "Popped: " << c << endl; // print during pop
+        cout << "Popped: " << c << endl; 
         return c;
     }
 
     bool isEmpty() {
         return top == nullptr;
+    }
+
+    bool isBalancedParentheses(const string& expression) {
+        // 
+        for (char c : expression) {
+          
+            if (c == '(' || c == '{' || c == '[') {
+                push(c);
+            }
+            
+            else if (c == ')' || c == '}' || c == ']') {
+              
+                if (isEmpty()) {
+                   
+                    while (top) pop(); 
+                    return false;
+                }
+                
+                char popped_char = top->data; t
+                
+                if (isMatchingPair(popped_char, c)) {
+                    
+                    pop(); 
+                } else {
+                    
+                    while (top) pop(); 
+                    return false;
+                }
+            }
+            
+        }
+
+        
+        bool balanced = isEmpty();
+        
+        
+        while (top) pop(); 
+
+        return balanced;
     }
 };
 
